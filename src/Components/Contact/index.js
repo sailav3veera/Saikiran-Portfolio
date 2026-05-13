b4d42e7745f9650f381aac230de3ff49a60b68b5
sailav3veera <sai123veera@gmail.com>
2026-05-13 10:54:40 +0530

    added form to contact

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,22 +1,26 @@
 import "./index.css";
+import ContactForm from "../ContactForm";
 
 function Contact() {
   return (
-    <div className="maincontainerforcontact">
-      <ul className="ulstyling">
-        <li className="liststyle">
-          <h1 className="headingcontact">Name :</h1>
-          <p> Sai kiran Veera</p>
-        </li>
-        <li className="liststyle">
-          <h1>Email :</h1>
-          <p> sai123veera@gmail.com</p>
-        </li>
-        <li className="liststyle">
-          <h1>Contact Number :</h1>
-          <p> 9515011066</p>
-        </li>
-      </ul>
+    <div className="forms">
+      <div className="maincontainerforcontact">
+        <ul className="ulstyling">
+          <li className="liststyle">
+            <h1 className="headingcontact">Name :</h1>
+            <p> Sai kiran Veera</p>
+          </li>
+          <li className="liststyle">
+            <h1>Email :</h1>
+            <p> sai123veera@gmail.com</p>
+          </li>
+          <li className="liststyle">
+            <h1>Contact Number :</h1>
+            <p> 9515011066</p>
+          </li>
+        </ul>
+      </div>
+      <ContactForm />
     </div>
   );
 }

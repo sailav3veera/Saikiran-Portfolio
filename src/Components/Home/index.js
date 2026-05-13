b4bb86b79518b5c4dec9cc86cb58957f8945040c
sailav3veera <sai123veera@gmail.com>
2026-05-13 11:05:31 +0530

    button added in homepage

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./index.css";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
@@ -8,7 +9,9 @@ class Home extends Component {
         <div className="subhomecontainer">
           <h1>Hi, I'm Sai kiran Veera</h1>
           <p>I am a web developer</p>
-          
+          <Link to="/about">
+            <button className="knowmorebutton">Know More</button>
+          </Link>
         </div>
         <img
           src="https://res.cloudinary.com/dhag7xryo/image/upload/v1778503016/ChatGPT_Image_May_11_2026_06_06_35_PM_btvzfu.png"

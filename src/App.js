0fcc8baad2382efeff6b55beba733567056b6b31
sailav3veera <sai123veera@gmail.com>
2026-06-01 12:06:43 +0530

    added project component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Header from "./Components/Header";
 import NotFound from "./Components/NotFound";
 import { Routes, BrowserRouter, Route, Navigate } from "react-router-dom";
 import About from "./Components/About";
-import Contact from "./Components/Contact";
+//import Contact from "./Components/Contact";
+//<Route path="/contact" element={<Contact />} />
+import Projects from "./Components/Projects";
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/PageNotFound" element={<NotFound />} />
         <Route path="*" element={<Navigate to="/PageNotFound" />} />
       </Routes>
---
+++ src/Components/Projects/index.js
@@ -0,0 +1,89 @@
+import { Component } from "react";
+import "./index.css";
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="pojectscontainer">
+        <h1>My Projects</h1>
+        <div
+          id="carouselExampleAutoplaying"
+          className="carousel slide maincon"
+          data-bs-ride="carousel"
+          data-bs-interval="2000"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active imagecontainer">
+              <img
+                src="https://res.cloudinary.com/dnoycfpby/image/upload/v1780295004/ChatGPT_Image_Jun_1_2026_11_53_10_AM_uqzdhy.png"
+                className="d-block slideshowimages"
+                alt="food delivery"
+              />
+              <a
+                href="https://food-delivery-application-saikiran-veera.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="buttonproject">Access the project</button>
+              </a>
+            </div>
+            <div className="carousel-item active imagecontainer">
+              <img
+                src="https://res.cloudinary.com/dnoycfpby/image/upload/v1780292706/stopwatch_zzt5jo.jpg"
+                className="d-block slideshowimages"
+                alt="stopwatch"
+              />
+              <a
+                href="https://veera3stopwatch.ccbp.tech/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="buttonproject">Access the project</button>
+              </a>
+            </div>
+            <div className="carousel-item active imagecontainer">
+              <img
+                src="https://res.cloudinary.com/dnoycfpby/image/upload/v1780295668/ChatGPT_Image_Jun_1_2026_12_04_08_PM_zt3ktg.png"
+                className="d-block slideshowimages"
+                alt="stopwatch"
+              />
+              <a
+                href="https://veerawatchapp.ccbp.tech/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="buttonproject">Access the project</button>
+              </a>
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Projects;
